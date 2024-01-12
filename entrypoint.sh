569e2f72684fb10531df01f7b32b727c5937926a
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2024-01-12 12:05:06 +0100

    SSL verify

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
-java $JAVA_OPTS -jar /app/keycloak-config-cli.jar $0 $@
+java $JAVA_OPTS -jar /app/keycloak-config-cli.jar \
+    --keycloak.url=${KEYCLOAK_URL} \
+    --keycloak.user=${KEYCLOAK_USER} \
+    --keycloak.password=${KEYCLOAK_PASSWORD} \
+    --import.files.locations=${IMPORT_FILES} \
+    --keycloak.ssl-verify=${SSL_VERIFY}
